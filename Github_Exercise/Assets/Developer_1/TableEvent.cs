4c2905cd8082f3311ec1445b4ec183ed5d76fa4a
Tis-coco <10510226@gm.nttu.edu.tw>
2021-02-27 18:24:58 +0800

    測試

---
+++ Github_Exercise/Assets/Developer_1/TableEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("RotationTable", 0,0.05f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void RotationTable() {
+        gameObject.GetComponent<Transform>().transform.Rotate(new Vector3(0,0,2));
+    }
+}
